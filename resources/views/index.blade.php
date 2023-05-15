2e712b37d81efece3deacffd6d39642915933fcc
AlbertoST876 <albertost876.ast@gmail.com>
2023-05-15 10:29:21 +0200

    Terminada vista Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,10 +12,10 @@
 
             <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-80 h-screen transition-transform -translate-x-full lg:translate-x-0" aria-label="Sidebar">
                 <div class="h-full overflow-y-auto bg-black">
-                    <a href="{{ route("index") }}" class="flex items-center justify-center mt-8 mb-2"><img src="{{ asset("icon.png") }}" class="h-64" alt="Operator's Cup Logo"></a>
+                    <a href="{{ route("index") }}" class="flex mt-8 mb-2 items-center justify-center"><img src="{{ asset("icon.png") }}" class="h-64" alt="Operator's Cup Logo"></a>
 
                     <div>
-                        <button id="languageButton" data-dropdown-toggle="language" class="flex mx-auto mb-8 px-4 py-2 items-center text-white font-medium rounded-lg text-sm text-center hover:bg-white hover:text-black focus:ring-4 focus:outline-none focus:ring-white" type="button">
+                        <button id="languageButton" data-dropdown-toggle="language" class="flex mx-auto px-4 py-2 items-center text-white font-medium rounded-lg text-sm text-center hover:bg-white hover:text-black focus:ring-4 focus:outline-none focus:ring-white" type="button">
                             @switch(session() -> get("locale"))
                                 @case("en") <img src="{{ asset("storage/images/flags/en.png") }}" width="25px"> @break
                                 @case("es") <img src="{{ asset("storage/images/flags/es.png") }}" width="25px"> @break
@@ -25,7 +25,7 @@
                             <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                         </button>
 
-                        <div id="language" class="z-10 hidden bg-white rounded-lg shadow w-44">
+                        <div id="language" class="hidden w-44 z-10 bg-white rounded-lg shadow">
                             <ul class="py-2 text-sm text-gray-700" aria-labelledby="languageButton">
                                 <li><a href="lang/en" class="block px-4 py-2 text-left hover:bg-gray-100"><img class="inline-block mr-2" src="{{ asset("storage/images/flags/en.png") }}" width="25px">@lang("app.english")</a></li>
                                 <li><a href="lang/es" class="block px-4 py-2 text-left hover:bg-gray-100"><img class="inline-block mr-2" src="{{ asset("storage/images/flags/es.png") }}" width="25px">@lang("app.spanish")</a></li>
@@ -33,7 +33,7 @@
                         </div>
                     </div>
 
-                    <ul class="space-y-2 font-bold">
+                    <ul class="my-8 space-y-2 font-bold">
                         <li class="transition-all duration-500 hover:bg-white"><a href="{{ route("index") }}" class="flex items-center px-8 py-4 text-xl text-white rounded-lg hover:text-black">HOME</a></li>
                         <li class="transition-all duration-500 hover:bg-white"><a href="{{ route("index") }}" class="flex items-center px-8 py-4 text-xl text-white rounded-lg hover:text-black">RULES</a></li>
                         <li class="transition-all duration-500 hover:bg-white"><a href="{{ route("index") }}" class="flex items-center px-8 py-4 text-xl text-white rounded-lg hover:text-black">TEAMS</a></li>
@@ -42,7 +42,7 @@
                         <li class="transition-all duration-500 hover:bg-white"><a href="{{ route("index") }}" class="flex items-center px-8 py-4 text-xl text-white rounded-lg hover:text-black">STATS</a></li>
                     </ul>
 
-                    <div class="flex px-8 py-4 items-center justify-between absolute left-0 right-0 bottom-4">
+                    <div class="flex px-8 py-4 items-center justify-between">
                         <a class="w-8 h-8" target="_blank" href="https://twitter.com/OperatorsCup"><img class="rounded-md" src="{{ asset("storage/images/social-networks/twitter.png") }}" alt="Twitter"></a>
                         <a class="w-8 h-8" target="_blank" href="https://twitch.tv/operatorscupr6"><img class="rounded-md" src="{{ asset("storage/images/social-networks/twitch.png") }}" alt="Twitch"></a>
                         <a class="w-8 h-8" target="_blank" href="https://youtube.com/@OperatorsCup"><img class="rounded-md" src="{{ asset("storage/images/social-networks/youtube.png") }}" alt="Youtube"></a>
@@ -54,18 +54,22 @@
         <main class="lg:ml-80 p-4">
             <h1 class="block text-center text-4xl sm:text-6xl font-black">OPERATOR'S CUP<br>SEASON 3</h1>
 
-            <div id="carousel" class="relative w-full my-8" data-carousel="slide">
-                <div class="relative h-36 sm:h-52 md:h-64 lg:h-72 xl:h-96 2xl:h-[500px] overflow-hidden rounded-lg">
+            <div id="carousel" class="relative w-auto my-8" data-carousel="slide">
+                <div class="relative h-36 sm:h-52 md:h-64 lg:h-60 xl:h-80 2xl:h-96 overflow-hidden rounded-lg">
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="{{ asset("storage/images/Banner1.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="{{ asset("storage/images/Banner1.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Operator's Cup Banner">
                     </div>
 
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="{{ asset("storage/images/Banner2.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="{{ asset("storage/images/Banner2.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Rainbow Six Siege">
                     </div>
 
                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="{{ asset("storage/images/Banner3.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="{{ asset("storage/images/Banner3.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Operation Commanding Force">
+                    </div>
+
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <img src="{{ asset("storage/images/Banner4.jpg") }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Brava">
                     </div>
                 </div>
 
@@ -73,33 +77,70 @@
                     <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
                     <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
                     <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
                 </div>
 
                 <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full border sm:w-10 sm:h-10 bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-black group-focus:outline-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
                         <span class="sr-only">Previous</span>
                     </span>
                 </button>
 
                 <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full border sm:w-10 sm:h-10 bg-black/30 group-hover:bg-black/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                         <span class="sr-only">Next</span>
                     </span>
                 </button>
             </div>
+
+            <div class="block w-full">
+                <h2 class="block my-4 text-2xl sm:text-4xl font-black">Novedades de Rainbow Six Siege</h2>
+
+                <article class="my-4">
+                    <h3 class="block my-4 text-xl sm:text-2xl font-black">Nueva Operación Commanding Force</h3>
+
+                    <p class="my-4 text-md text-justify">
+                        La <strong>Operation Commanding Force</strong> trae a <strong>Brava</strong>, una nueva atacante brasileña, una
+                        importante función para abordar el uso de ratón y teclado en consolas, un sistema de recarga inmersiva, nuevas
+                        medidas contra el comportamiento tóxico, etc.
+                    </p>
+                </article>
+
+                <article class="my-4">
+                    <h3 class="block my-4 text-xl sm:text-2xl font-black">Nueva Operadora Brava</h3>
+
+                    <div class="flex items-center">
+                        <img class="block w-56 mr-8" src="{{ asset("storage/images/Brava.png") }}" alt="Brava">
+
+                        <div>
+                            <p class="my-4 text-md text-justify">
+                                <strong>Nayara Brava Cardoso</strong>, es una operadora atacante introducida en la Operación Commanding
+                                Force.
+                            </p>
+
+                            <p class="my-4 text-md text-justify">
+                                El dispositivo único de <strong>Brava</strong> es el <strong>Kludge Drone</strong>, un dron remoto
+                                equipado con un láser electrónico. Cuando está dentro del alcance y la línea de visión de un dispositivo
+                                electrónico de los defensores, Brava puede piratear el dispositivo, ya sea haciendo que se autodestruya
+                                o usando sus efectos en contra de los defensores, que de otro modo no se verían afectados.
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            </div>
         </main>
 
-        <footer class="lg:ml-80 p-4 bg-gray-100 rounded-lg shadow absolute left-0 right-0 bottom-0">
+        <footer class="block mt-8 lg:ml-80 p-4 bg-gray-100 rounded-lg shadow">
             <div class="block w-full md:flex md:items-center md:justify-between">
                 <span class="text-lg font-medium text-black sm:text-center">&copy;{{ date("Y") }} <a href="{{ route("index") }}" class="hover:underline">Operator's Cup</a>. All Rights Reserved.</span>
 
                 <ul class="flex flex-wrap items-center mt-3 text-lg font-medium text-black sm:mt-0">
-                    <li><a href="#" class="mr-4 hover:underline md:mr-6 ">About</a></li>
-                    <li><a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a></li>
-                    <li><a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a></li>
-                    <li><a href="#" class="hover:underline">Contact</a></li>
+                    <li><a href="{{ route("index") }}" class="mr-4 md:mr-6 hover:underline">About</a></li>
+                    <li><a href="{{ route("index") }}" class="mr-4 md:mr-6 hover:underline">Privacy Policy</a></li>
+                    <li><a href="{{ route("index") }}" class="mr-4 md:mr-6 hover:underline">Licensing</a></li>
+                    <li><a href="{{ route("index") }}" class="hover:underline">Contact</a></li>
                 </ul>
             </div>
 
