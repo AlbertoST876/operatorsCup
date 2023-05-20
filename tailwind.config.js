c6ef14cf260887f2aa7615c4871cc39a3826f770
AlbertoST876 <albertost876.ast@gmail.com>
2023-05-20 19:50:17 +0200

    Comillas dobles en la configuración de vite y arregladas tabulaciones en la configuración de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
-/** @type {import("tailwindcss").Config} */
+/**
+ * @type { import("tailwindcss").Config }
+ */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("flowbite/plugin"),
+    ],
 }
 
