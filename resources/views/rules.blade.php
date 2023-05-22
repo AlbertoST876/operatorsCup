5d75d5d4c2a2b8df096b7792912131fb910e2d99
AlbertoST876 <albertost876.ast@gmail.com>
2023-05-22 08:13:56 +0200

    Modificados estilos de rules y header

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -3,13 +3,11 @@
 <html lang="{{ app() -> getLocale() }}">
     @include("layouts.head", ["title" => __("app.rules")])
 
-    <body>
-        <div class="grid grid-rows-[auto,1fr] h-screen">
-            @include("layouts.header")
+    <body class="grid grid-rows-[auto,1fr] h-screen">
+        @include("layouts.header")
 
-            <main>
-                <embed src="{{ asset("storage/pdf/Rules_" . mb_strtoupper(app() -> getLocale()) . ".pdf") }}" type="application/pdf" width="100%" height="100%" frameborder="0">
-            </main>
-        </div>
+        <main>
+            <embed src="{{ asset("storage/pdf/Rules_" . mb_strtoupper(app() -> getLocale()) . ".pdf") }}" type="application/pdf" width="100%" height="100%" frameborder="0">
+        </main>
     </body>
 </html>
