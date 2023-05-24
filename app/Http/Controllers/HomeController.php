4672da756ec160d298144ef482048417b1432fe4
AlbertoST876 <albertost876.ast@gmail.com>
2023-05-24 16:43:30 +0200

    Terminada vista ranking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Team;
+use App\Models\Set;
 
 class HomeController extends Controller
 {
@@ -28,23 +30,98 @@ class HomeController extends Controller
     }
 
     /**
-     * Return view calendar
-     *
-     * @return Response
-     */
-    public function calendar()
-    {
-        return view("calendar");
-    }
-
-    /**
      * Return view ranking
      *
      * @return Response
      */
     public function ranking()
     {
-        return view("ranking");
+        $teamsDB = Team::select("id", "name", "logo", "active") -> get();
+        $teams = [];
+
+        foreach ($teamsDB as $team)
+        {
+            $games = Set::leftJoin("games", "sets.id", "games.set") -> where("sets.winner", $team -> id) -> orWhere("sets.loser", $team -> id) -> where("sets.workday", "<", 10) -> select("games.winner", "games.overtime",  "games.wResult", "games.lResult") -> get();
+            $points = 0;
+            $wins = 0;
+            $loses = 0;
+            $winsOvertime = 0;
+            $losesOvertime = 0;
+            $roundsWin = 0;
+            $roundsLose = 0;
+
+            foreach ($games as $game)
+            {
+                if ($game -> overtime)
+                {
+                    if ($game -> winner == $team -> id)
+                    {
+                        $winsOvertime++;
+                        $points += 2;
+                    }
+                    else
+                    {
+                        $losesOvertime++;
+                        $points++;
+                    }
+                }
+                else
+                {
+                    if ($game -> winner == $team -> id)
+                    {
+                        $wins++;
+                        $points += 3;
+                    }
+                    else
+                    {
+                        $loses++;
+                    }
+                }
+
+                if ($game -> winner == $team -> id)
+                {
+                    $roundsWin += $game -> wResult;
+                    $roundsLose += $game -> lResult;
+                }
+                else
+                {
+                    $roundsWin += $game -> lResult;
+                    $roundsLose += $game -> wResult;
+                }
+            }
+
+            $teams[] = [
+                "info" => $team,
+                "points" => $points,
+                "wins" => $wins,
+                "loses" => $loses,
+                "winsOvertime" => $winsOvertime,
+                "losesOvertime" => $losesOvertime,
+                "roundsDiff" => $roundsWin - $roundsLose,
+            ];
+        }
+
+        usort($teams, function($teamA, $teamB) {
+            if ($teamA["points"] == $teamB["points"])
+            {
+                return 0;
+            }
+
+            return ($teamA["points"] > $teamB["points"]) ? -1 : 1;
+        });
+
+        usort($teams, function($teamA, $teamB) {
+            if ($teamA["roundsDiff"] == $teamB["roundsDiff"])
+            {
+                return 0;
+            }
+
+            return ($teamA["roundsDiff"] > $teamB["roundsDiff"]) ? -1 : 1;
+        });
+
+        return view("ranking", [
+            "teams" => $teams,
+        ]);
     }
 
     /**
--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -10,9 +10,44 @@
             <div class="max-w-screen-xl mx-auto">
                 <h1 class="block my-8 text-4xl sm:text-6xl font-black">{{ mb_strtoupper(__("app.ranking")) }}</h1>
 
-                <hr>
+                <table class="block w-full mt-16 overflow-x-scroll">
+                    <thead class="border-b-2 border-black">
+                        <tr>
+                            <th class="w-[50px] md:w-[100px] p-4"></th>
+                            <th class="w-[50px] md:w-[500px] p-4"></th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Puntos</th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Ganados</th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Ganados Overtime</th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Perdidos Overtime</th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Perdidos</th>
+                            <th class="w-[50px] md:w-[100px] p-4 text-xl text-center font-bold">Diferencia Rondas</th>
+                        </tr>
+                    </thead>
 
+                    <tbody>
+                        @php $rank = 1; @endphp
 
+                        @foreach ($teams as $team)
+                            <tr class="border-b">
+                                <td class="w-[50px] md:w-[100px] p-4 text-3xl font-bold">{{ "#" . $rank++ }}</td>
+
+                                <td class="w-[300px] md:w-[500px] p-4">
+                                    <a class="inline-flex flex-nowrap items-center" href="{{ route("teams.show", $team["info"] -> id) }}">
+                                        <img class="mr-4 w-24 h-24" src="{{ asset($team["info"] -> logo) }}" alt="{{ $team["info"] -> name }} Logo">
+                                        <span class="text-2xl sm:text-4xl font-black">{{ $team["info"] -> name }}</span>
+                                    </a>
+                                </td>
+
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["points"] }}</td>
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["wins"] }}</td>
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["winsOvertime"] }}</td>
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["losesOvertime"] }}</td>
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["loses"] }}</td>
+                                <td class="w-[50px] md:w-[100px] p-4 text-2xl sm:text-4xl text-center font-bold">{{ $team["roundsDiff"] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </main>
 
