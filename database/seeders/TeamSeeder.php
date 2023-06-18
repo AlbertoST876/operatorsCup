6494946d77d237e4bd733477a58657f0c0a10496
AlbertoST876 <albertost876.ast@gmail.com>
2023-06-18 14:37:03 +0200

    Cambio de equipo de BlackHole a Alfa Wolf

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -14,14 +14,14 @@ class TeamSeeder extends Seeder
     public function run(): void
     {
         $teams = [
-            ["Xperience",       "XP",   "storage/images/teams/XP.png",   "Plainairplane48@gmail.com",  "692979083",   null, "https://twitter.com/Xperiencexp1",   null, null, 1],
-            ["Luna Esport",     "LUNA", "storage/images/teams/LUNA.png", "forniermathis1@gmail.com",   "0673855127",  null, "https://twitter.com/Luna__Esport",   null, null, 1],
-            ["ZeroPain",        "ZP",   "storage/images/teams/ZP.png",   "fvaldiviaa08@gmail.com",     "639934132",   null, null,                                 null, null, 1],
-            ["ETERNITY",        "ETY",  "storage/images/teams/ETY.png",  "mariomannina1@gmail.com",    "3279807061",  null, null,                                 null, null, 1],
-            ["Benny Boys",      "BB",   "storage/images/teams/BB.png",   "raw12341@outlook.com",       "07456893602", null, "https://twitter.com/Bennysboys_LFO", null, null, 1],
-            ["Guardians Alpha", "GRDX", "storage/images/teams/GRDX.png", "edgarsjemeljanovs@inbox.lv", "123456789",   null, "https://twitter.com/bRainbowTeam",   null, null, 1],
-            ["BlackholeSport",  "BH", "storage/images/teams/BH.png",     "jfvfernandez@icloud.com",    "657405121",   null, null,                                 null, null, 1],
-            ["Astony",          "ASTO", "storage/images/teams/ASTO.png", "jens.aerts.1998@gmail.com",  "0493615483",  null, "https://twitter.com/TeamASTONY",     null, null, 1],
+            ["Xperience",        "XP",   "storage/images/teams/XP.png",   "Plainairplane48@gmail.com",  "692979083",   null, "https://twitter.com/Xperiencexp1",   null, null, 1],
+            ["Luna Esport",      "LUNA", "storage/images/teams/LUNA.png", "forniermathis1@gmail.com",   "0673855127",  null, "https://twitter.com/Luna__Esport",   null, null, 1],
+            ["ZeroPain",         "ZP",   "storage/images/teams/ZP.png",   "fvaldiviaa08@gmail.com",     "639934132",   null, null,                                 null, null, 1],
+            ["ETERNITY",         "ETY",  "storage/images/teams/ETY.png",  "mariomannina1@gmail.com",    "3279807061",  null, null,                                 null, null, 1],
+            ["Benny Boys",       "BB",   "storage/images/teams/BB.png",   "raw12341@outlook.com",       "07456893602", null, "https://twitter.com/Bennysboys_LFO", null, null, 1],
+            ["Guardians Alpha",  "GRDX", "storage/images/teams/GRDX.png", "edgarsjemeljanovs@inbox.lv", "123456789",   null, "https://twitter.com/bRainbowTeam",   null, null, 1],
+            ["Alfa Wolf eSport", "AWS",  "storage/images/teams/AWS.png",  "jfvfernandez@icloud.com",    "657405121",   null, null,                                 null, null, 1],
+            ["Astony",           "ASTO", "storage/images/teams/ASTO.png", "jens.aerts.1998@gmail.com",  "0493615483",  null, "https://twitter.com/TeamASTONY",     null, null, 1],
         ];
 
         foreach ($teams as $team)
