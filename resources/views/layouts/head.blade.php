e63a877e4f775310a88cb0c0154a547b2a8be275
AlbertoST876 <albertosancheztorreblanca@gmail.com>
2024-11-08 00:48:15 +0100

    Quitado el rastreo de los indexadores

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="Alberto Sánchez Torreblanca">
     <meta name="copyright" content="Alberto Sánchez Torreblanca">
-    <meta name="robots" content="index,follow">
+    <meta name="robots" content="noindex,nofollow">
     @vite(["resources/js/app.js", "resources/css/app.css"])
     <link rel="icon" href="{{ asset("icon.png") }}">
     <title>{{ config("app.name") }} - {{ $title }}</title>
